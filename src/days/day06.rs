c593be2634fcbf85647098d2cc8fd207a36e979a
Rob Wells <rob@robjwells.com>
2024-12-16 13:10:51 +0000

    Day 6: part two, with foldhash

--- src/days/day06.rs
+++ src/days/day06.rs
@@ -1,4 +1,4 @@
-use std::collections::BTreeSet;
+use std::collections::HashSet;
 use std::str::FromStr;
 
 use crate::util::Answer;
@@ -9,7 +9,14 @@ pub fn solve(input: &str) -> anyhow::Result<String> {
     let p1 = count_visited_positions(&grid);
     assert_eq!(p1, 4722, "Part one is not the correct answer.");
 
-    Answer::first(6, p1).report()
+    let p2 = count_loops_with_new_walls(&grid);
+    assert_eq!(p2, 1602, "Part two is not the correct answer.");
+
+    Answer::first(6, p1).second(p2).report()
+}
+
+fn new_hashset<T>(capacity: usize) -> HashSet<T, foldhash::fast::RandomState> {
+    HashSet::with_capacity_and_hasher(capacity, foldhash::fast::RandomState::default())
 }
 
 #[derive(Debug, PartialEq, Eq, PartialOrd, Ord, Clone, Copy, Hash)]
@@ -24,7 +31,7 @@ impl Position {
     }
 }
 
-#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
 enum Direction {
     North,
     East,
@@ -44,7 +51,7 @@ impl Direction {
 }
 
 struct Grid {
-    walls: BTreeSet<Position>,
+    walls: HashSet<Position, foldhash::fast::RandomState>,
     start_position: Position,
     n_rows: usize,
     n_cols: usize,
@@ -59,11 +66,7 @@ impl Grid {
         position.row < self.n_rows && position.col < self.n_cols
     }
 
-    fn next_position(
-        &self,
-        position: Position,
-        direction: Direction,
-    ) -> Option<(Position, Direction)> {
+    fn move_one(&self, position: Position, direction: Direction) -> Option<Position> {
         let Position { row, col } = position;
         let next_pos = match direction {
             Direction::North => (row.checked_sub(1), Some(col)),
@@ -75,13 +78,35 @@ impl Grid {
             return None;
         };
         let next_pos = Position::new(next_row, next_col);
-        if !self.in_bounds(next_pos) {
-            return None;
-        }
+        self.in_bounds(next_pos).then_some(next_pos)
+    }
+
+    fn next_position(
+        &self,
+        position: Position,
+        direction: Direction,
+    ) -> Option<(Position, Direction)> {
+        let next_pos = self.move_one(position, direction)?;
         if self.walls.contains(&next_pos) {
             return self.next_position(position, direction.turn_right());
         }
         Some((next_pos, direction))
+    }
+
+    fn next_position_at_wall(
+        &self,
+        extra_wall: Position,
+        position: Position,
+        direction: Direction,
+    ) -> Option<(Position, Direction)> {
+        let mut pos = position;
+        while let Some(next_pos) = self.move_one(pos, direction) {
+            if next_pos == extra_wall || self.walls.contains(&next_pos) {
+                return Some((pos, direction.turn_right()));
+            }
+            pos = next_pos;
+        }
+        None
     }
 }
 
@@ -90,7 +115,7 @@ impl FromStr for Grid {
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let mut start: Option<Position> = None;
-        let mut walls: BTreeSet<Position> = BTreeSet::new();
+        let mut walls = new_hashset(1024);
 
         let n_rows = s.lines().count();
         let n_cols = s
@@ -119,7 +144,8 @@ impl FromStr for Grid {
 
 fn count_visited_positions(grid: &Grid) -> usize {
     let (mut pos, mut dir) = grid.start_position();
-    let mut visited = BTreeSet::from([pos]);
+    let mut visited = new_hashset(4800);
+    visited.insert(pos);
     while let Some(next) = grid.next_position(pos, dir) {
         (pos, dir) = next;
         visited.insert(pos);
@@ -127,9 +153,41 @@ fn count_visited_positions(grid: &Grid) -> usize {
     visited.len()
 }
 
+fn count_loops_with_new_walls(grid: &Grid) -> u32 {
+    let (mut pos, mut dir) = grid.start_position();
+    let mut visited = new_hashset(4800);
+    visited.insert(pos);
+    let mut loops_found = 0;
+    while let Some((next_pos, next_dir)) = grid.next_position(pos, dir) {
+        // Checking the position only is fine (rather than (position, direction))
+        // because if there's a wall there, it's there from the start, so
+        // only the first encounter matters.
+        if !visited.contains(&next_pos) && check_for_loop(grid, next_pos, (pos, dir)) {
+            loops_found += 1;
+        }
+        (pos, dir) = (next_pos, next_dir);
+        visited.insert(pos);
+    }
+    loops_found
+}
+
+fn check_for_loop(grid: &Grid, extra_wall: Position, start_at: (Position, Direction)) -> bool {
+    let mut visited = new_hashset(4800);
+    visited.insert(start_at);
+    let (mut pos, mut dir) = start_at;
+    while let Some(next) = grid.next_position_at_wall(extra_wall, pos, dir) {
+        if visited.contains(&next) {
+            return true;
+        }
+        (pos, dir) = next;
+        visited.insert(next);
+    }
+    false
+}
+
 #[cfg(test)]
 mod test {
-    use super::{count_visited_positions, Direction, Grid, Position};
+    use super::{count_loops_with_new_walls, count_visited_positions, Direction, Grid, Position};
     use rstest::{fixture, rstest};
 
     const SAMPLE_INPUT: &str = "\
@@ -169,6 +227,15 @@ mod test {
         let grid: Grid = SAMPLE_INPUT.parse()?;
         let n_visited = count_visited_positions(&grid);
         assert_eq!(n_visited, 41);
+        Ok(())
+    }
+
+    #[rstest]
+    #[allow(unused_variables)]
+    fn sample_grid_find_loops(setup_tracing: &()) -> anyhow::Result<()> {
+        let grid: Grid = SAMPLE_INPUT.parse()?;
+        let n_loops = count_loops_with_new_walls(&grid);
+        assert_eq!(n_loops, 6);
         Ok(())
     }
 }
